dd658349672518a3cfaa4edb9fe205690e880328
Mark <mvega37@cnm.edu>
2026-03-03 16:01:54 -0700

    Refactored `PassphraseController` for Thymeleaf support, added `ExceptionMapper` for validation errors, and introduced HTML template for passphrase display.

--- src/main/java/edu/cnm/deepdive/diceware/controller/PassphraseController.java
+++ src/main/java/edu/cnm/deepdive/diceware/controller/PassphraseController.java
@@ -3,18 +3,20 @@ package edu.cnm.deepdive.diceware.controller;
 import edu.cnm.deepdive.diceware.service.PassphraseService;
 import jakarta.validation.constraints.Max;
 import java.util.List;
-import java.util.stream.Collectors;
 import org.hibernate.validator.constraints.Length;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-@RestController
-@RequestMapping("/passphrase")
+@Controller
+@RequestMapping("/passphrases")
 @Validated
 public class PassphraseController {
 
@@ -26,6 +28,7 @@ public class PassphraseController {
   }
 
   @PostMapping(path = "/generate", produces = MediaType.APPLICATION_JSON_VALUE)
+  @ResponseBody
   public List<String> post(
       @Max(20)
       @RequestParam(defaultValue = "5")
@@ -35,15 +38,28 @@ public class PassphraseController {
   }
 
   @PostMapping(path = "/generate", produces = MediaType.TEXT_PLAIN_VALUE)
+  @ResponseBody
   public String post(
       @Max(20)
       @RequestParam(defaultValue = "5")
       int length,
       @Length(max = 5)
       @RequestParam(defaultValue = " ")
-          String delimiter
+      String delimiter
   ) {
-    return String.join(delimiter, service
-        .generate(length));
+    return String.join(delimiter, service.generate(length));
   }
+
+  @GetMapping(path = "/passphrase")
+  public String get(
+      @Max(20)
+      @RequestParam(defaultValue = "5")
+      int length,
+      Model model
+  ) {
+    model.addAttribute("words", service.generate(length));
+    return "passphrase";
+  }
+
+
 }
